5fab42bbba38bbf9a5b85ba774752188010b266e
KolesnikovPavel <KolesnikovPavel32@gmail.com>
2020-02-09 17:46:19 +0500

    Migrations added to repository

---
+++ Migrations/20200208194031_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scheduler.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inspections",
+                columns: table => new
+                {
+                    InspectionId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Classifier = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Date = table.Column<string>(nullable: true),
+                    Time = table.Column<int>(nullable: false),
+                    ActingInspector = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inspections", x => x.InspectionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inspectors",
+                columns: table => new
+                {
+                    InspectorId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Specialization = table.Column<string>(nullable: true),
+                    Preferences = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    VacationStart = table.Column<string>(nullable: true),
+                    VacationEnd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inspectors", x => x.InspectorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inspections");
+
+            migrationBuilder.DropTable(
+                name: "Inspectors");
+        }
+    }
+}
